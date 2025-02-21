ca1469aac54abea4823dd33d55fcf9017ae665fe
MercuryAtom31 <83618354+MercuryAtom31@users.noreply.github.com>
2025-02-20 22:02:43 -0500

    Changed the schema creation keywords

--- backend/portfolio/src/main/resources/schema.sql
+++ backend/portfolio/src/main/resources/schema.sql
@@ -1,19 +1,39 @@
+-- Do not drop tables to preserve data
 -- DROP TABLE IF EXISTS users;
-DROP TABLE IF NOT EXISTS users;
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL
 );
 
 -- DROP TABLE IF EXISTS testimonials;
-DROP TABLE IF NOT EXISTS testimonials;
-CREATE TABLE testimonials (
+CREATE TABLE IF NOT EXISTS testimonials (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_name VARCHAR(100) NOT NULL,
     message TEXT NOT NULL,
     approved BOOLEAN DEFAULT FALSE
 );
+
+
+
+
+
+-- -- DROP TABLE IF EXISTS users;
+-- DROP TABLE IF NOT EXISTS users;
+-- CREATE TABLE users (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     username VARCHAR(50) NOT NULL,
+--     email VARCHAR(100) NOT NULL
+-- );
+
+-- -- DROP TABLE IF EXISTS testimonials;
+-- DROP TABLE IF NOT EXISTS testimonials;
+-- CREATE TABLE testimonials (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     user_name VARCHAR(100) NOT NULL,
+--     message TEXT NOT NULL,
+--     approved BOOLEAN DEFAULT FALSE
+-- );
 
 
 
